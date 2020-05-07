36f179032c1fda03f45cf6f183d565b854366d52
cece <cc950114@163.com>
2020-05-07 14:52:20 +0800

    编写局部变量i

--- git_test/src/cece/git/GitTest.java
+++ git_test/src/cece/git/GitTest.java
@@ -9,4 +9,10 @@ public class GitTest {
         System.out.println("hello git...");
         System.out.println("hello git...");
     }
+
+    public static void test() {
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
 }
