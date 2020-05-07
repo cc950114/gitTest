af90d36364b697b70e6c6483e12fbd9ab61563cd
cece <cc950114@163.com>
2020-05-07 14:50:51 +0800

    本地工程添加到git

---
+++ git_test/src/cece/git/GitTest.java
@@ -0,0 +1,12 @@
+package cece.git;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+        System.out.println("hello git...");
+    }
+}
